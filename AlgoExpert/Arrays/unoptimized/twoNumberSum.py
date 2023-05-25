821c9f7c597f0c3ec9b407ce22a02a548525b759
Vidyadhar Reddy <vidyadhar.bth@gmail.com>
2023-05-25 17:43:53 +0000

    directory change

---
+++ AlgoExpert/Arrays/unoptimized/twoNumberSum.py
@@ -0,0 +1,15 @@
+class Solution:
+    def twoNumberSum(self, array, targetSum):
+        num_set = set(array)
+        for num in num_set:
+            difference = targetSum - num
+            if difference in array and difference != num:
+                return [num, difference]
+        return []
+
+solution = Solution()
+
+"""
+testcase-1
+print(solution.twoNumberSum(array=[3, 5, -4, 8, 11, 1, -1, 6], targetSum=10))
+"""
---
+++ AlgoExpert/Arrays/unoptimized/uo-sortedSquaredArray.py
@@ -0,0 +1,6 @@
+def sortedSquaredArray(array):
+    return sorted([x*x for x in array])
+
+"""
+time complexity of sort function is O(nlogn)
+"""
