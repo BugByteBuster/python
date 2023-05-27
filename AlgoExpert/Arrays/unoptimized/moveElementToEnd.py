2ff9021acc53d7ffe08c09eaa034bf35f3f3b97f
Vidyadhar Reddy <vidyadhar.bth@gmail.com>
2023-05-27 13:14:13 +0000

    moveElementToEnd unoptimized

---
+++ AlgoExpert/Arrays/unoptimized/moveElementToEnd.py
@@ -0,0 +1,16 @@
+#Un-optimized: O(n) time complexity
+def moveElementToEnd(array, toMove):
+    move_end_array = []
+    occurrence = array.count(toMove)
+    
+    if occurrence == 0:
+        return array
+    
+    for i in array:
+        if i != toMove:
+            move_end_array.append(i)
+    
+    for i in range(occurrence):
+        move_end_array.append(toMove)
+    
+    return move_end_array
