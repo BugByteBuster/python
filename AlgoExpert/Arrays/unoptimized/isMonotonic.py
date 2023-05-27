4871a6b093b7928211a1fd5dfdc59e6a3b1db5a5
Vidyadhar Reddy <vidyadhar.bth@gmail.com>
2023-05-27 16:14:19 +0000

    monotonic

--- AlgoExpert/Arrays/unoptimized/isMonotonic.py
+++ AlgoExpert/Arrays/unoptimized/isMonotonic.py
@@ -1,8 +1,7 @@
 #lifted and optimized
 def isMonotonic(array):
-    a = True
-    b = True
+    increasing = decreasing = True
     for i in range(len(array) - 1):
-        a = a and array[i] < array[i + 1]
-        b = b and array[i] > array[i + 1]
-    return a or b
+        increasing = increasing and array[i] <= array[i + 1]
+        decreasing = decreasing and array[i] >= array[i + 1]
+    return increasing or decreasing
