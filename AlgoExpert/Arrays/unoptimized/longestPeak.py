489d02e06acef50038086b963f9e8421c4e734e7
Vidyadhar Reddy <vidyadhar.bth@gmail.com>
2023-05-28 21:13:08 +0000

    longest peak length

---
+++ AlgoExpert/Arrays/unoptimized/longestPeak.py
@@ -0,0 +1,20 @@
+def longestPeak(array):
+    array_len = []
+    for i in range(1, len(array) - 1):
+        result_array = []
+        if array[i] > array[i-1] and array[i] > array[i+1]:
+
+            leftIndex = i - 2
+            while leftIndex >= 0 and array[leftIndex] < array[leftIndex + 1]:
+                result_array.append(array[leftIndex])
+                leftIndex -= 1 
+            
+            rightIndex = i + 2
+            while rightIndex < len(array) and array[rightIndex] < array[rightIndex - 1]:
+                result_array.append(array[rightIndex])
+                rightIndex += 1
+            array_len.append(len(result_array))
+    if len(array_len) > 0:
+        return max(array_len) + 3
+    else:
+        return 0  
