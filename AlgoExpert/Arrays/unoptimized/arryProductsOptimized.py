f791d8a4cfffe2584e9b5694a6d410bcf370e42d
Vidyadhar Reddy <vidyadhar.bth@gmail.com>
2023-05-29 23:45:06 +0000

    optimized arryOfProducts

---
+++ AlgoExpert/Arrays/unoptimized/arryProductsOptimized.py
@@ -0,0 +1,14 @@
+def arrayOfProducts(array):
+    result_array = [1] * len(array)  # Initialize result_array with 1s
+    
+    product = 1
+    for i in range(len(array)):
+        result_array[i] *= product  # Product of elements to the left
+        product *= array[i]
+    
+    product = 1
+    for i in range(len(array) - 1, -1, -1):
+        result_array[i] *= product  # Product of elements to the right
+        product *= array[i]
+    
+    return result_array
