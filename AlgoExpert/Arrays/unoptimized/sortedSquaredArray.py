2fe111dfc37d948f8043c9145032ad210a2a4d87
Vidyadhar Reddy <vidyadhar.bth@gmail.com>
2023-06-24 13:26:51 +0000

    O(n) timecomplexity

--- AlgoExpert/Arrays/unoptimized/sortedSquaredArray.py
+++ AlgoExpert/Arrays/unoptimized/sortedSquaredArray.py
@@ -1,10 +1,18 @@
 def sortedSquaredArray(array):
-    multiplier = 0
-    result = []
-    while multiplier < len(array):
-        result.append(array[multiplier] * array[multiplier])
-        multiplier = multiplier + 1
-    return sorted(result)
+    init = [0 for _ in array]
 
+    length = len(init)
+    smaller_index = 0
+    larger_index = length - 1
 
-sortedSquaredArray([1, 2, 3, 5, 6, 8, 9])
+    for idx in reversed(range(length)):
+        small_value = array[smaller_index]
+        large_value = array[larger_index]
+
+        if abs(small_value) > abs(large_value):
+            init[idx] = small_value * small_value
+            smaller_index += 1
+        else:
+            init[idx] = large_value * large_value
+            larger_index -= 1
+    return init
