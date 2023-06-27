3bd23bad8bc9d426f7bd9a92be3413cb9b0515cc
Vidyadhar Reddy <vidyadhar.bth@gmail.com>
2023-06-27 23:26:52 +0000

    validate sequence using sets

---
+++ AlgoExpert/easy/arrays/TwoNumberSum.py
@@ -0,0 +1,10 @@
+def twoNumberSum(array, targetSum):
+    uniqueNumbers = set(array)
+    for firstNumber in uniqueNumbers:
+        secondNumber = targetSum - firstNumber
+        if secondNumber in uniqueNumbers and secondNumber != firstNumber:
+            return [firstNumber, secondNumber]
+    return []
+
+
+twoNumberSum(array=[3, 5, -4, 8, 11, 1, -1, 6], targetSum=2)
