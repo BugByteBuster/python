0cf7afa36e66981e309290319e8ff01b90a0025a
Vidyadhar Reddy <vidyadhar.bth@gmail.com>
2023-06-30 17:07:55 +0000

    non constructible change

---
+++ AlgoExpert/easy/arrays/nonConstructibleChange.py
@@ -0,0 +1,12 @@
+def nonConstructibleChange(coins):
+    current_change = 0
+    sorted_coins = sorted(coins)
+    for coin in sorted_coins:
+        if coin > current_change + 1:
+            return current_change + 1
+        else:
+            current_change = current_change + coin
+    return current_change + 1
+
+
+nonConstructibleChange(coins=[5, 7, 1, 1, 2, 3, 22])
