3c2eafcce942de0335bd7e000acea38bc0347c1c
Vidyadhar Reddy <vidyadhar.bth@gmail.com>
2023-07-03 14:36:30 +0000

    palindrome

--- AlgoExpert/easy/strings/isPalindrome.py
+++ AlgoExpert/easy/strings/isPalindrome.py
@@ -1,3 +1,4 @@
+# o(n)
 def isPalindrome(string):
     rightIdx = len(string) - 1
     leftIdx = 0
@@ -8,3 +9,11 @@ def isPalindrome(string):
         leftIdx = leftIdx + 1
         rightIdx = rightIdx - 1
     return True
+
+
+# o(n^2)
+def isPalindrome2(string):
+    reverseString = ""
+    for i in reversed(range(len(string))):
+        reverseString = reverseString + string[i]
+    return reverseString == string
